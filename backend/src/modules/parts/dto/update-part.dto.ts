18b00eaa5852543d0cbd7ad68d4500d94a354146
Thiago da Paz <thiagopazjunior@gmail.com>
2023-11-24 08:33:58 -0300

    feat: Create UpdatePartDto

--- backend/src/modules/parts/dto/update-part.dto.ts
+++ backend/src/modules/parts/dto/update-part.dto.ts
@@ -1,4 +1,18 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePartDto } from './create-part.dto';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
-export class UpdatePartDto extends PartialType(CreatePartDto) {}
+export class UpdatePartDto extends PartialType(CreatePartDto) {
+  @IsNotEmpty()
+  @IsNumber()
+  id: string;
+
+  @IsString()
+  name: string;
+
+  @IsNumber()
+  brand: string;
+
+  @IsNumber()
+  price: number;
+}
